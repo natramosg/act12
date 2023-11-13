28f9bf5763253fe8c5e8b0a22d9696eefef3fc11
natramosg <146899538+natramosg@users.noreply.github.com>
2023-11-12 23:34:12 -0600

    Add files via upload

---
+++ perfil.js
@@ -0,0 +1,30 @@
+let studentName = "Natalia Ramos";
+let studentAge = 20;
+let isEnrolled = true;
+
+let gradeMath = 90;
+let gradeScience = 85;
+let gradeLanguage = 95;
+let totalClasses = 100;
+let attendedClasses = 80;
+
+let gradeMath = 90;
+let gradeScience = 85;
+let gradeLanguage = 95;
+let totalClasses = 100;
+let attendedClasses = 80;
+
+let gradeMath = 90;
+let gradeScience = 85;
+let gradeLanguage = 95;
+let totalClasses = 100;
+let attendedClasses = 80;
+
+console.log("Calificación Matemáticas: " + gradeMath);
+console.log("Calificación Ciencias: " + gradeScience);
+console.log("Calificación Lenguaje: " + gradeLanguage);
+console.log("Promedio de Calificaciones: " + averageGrade);
+
+console.log("Total de Clases: " + totalClasses);
+console.log("Clases Asistidas: " + attendedClasses);
+console.log("Porcentaje de Asistencia: " + attendancePercentage + "%");
---
+++ pt1.js
@@ -0,0 +1,13 @@
+let variableLet = "Hola, soy una variable let";
+console.log(variableLet);
+
+const variableConst = "Hola, soy una variable const";
+console.log(variableConst);
+
+var variableVar = "Hola, soy una variable var";
+console.log(variableVar);
+
+function demostrarScope() {
+    let variableLetFuncion = "Variable let dentro de la función";
+    console.log(variableLetFuncion);
+
